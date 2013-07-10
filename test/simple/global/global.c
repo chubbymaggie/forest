965dec9f1f81cda1aa3f60fdd9bf966bb0c84fa6
mint <mint@17f328a0-2faa-4260-a3e8-86cdd9e0d718>
2013-07-10 14:49:01 +0000

    Prueba que las variables globales no se inicializan
    git-svn-id: file:///media/disk/repository/release/trunk@203 17f328a0-2faa-4260-a3e8-86cdd9e0d718

---
+++ test/simple/global/global.c
@@ -0,0 +1,28 @@
+/*
+ * =====================================================================================
+ * /
+ * |     Filename:  global.c
+ * |
+ * |  Description:  
+ * |
+ * |      Version:  1.0
+ * |      Created:  07/10/2013 02:46:00 PM
+ * |     Revision:  none
+ * |     Compiler:  gcc
+ * `-. .--------------------
+ *    Y
+ *    ,,  ,---,
+ *   (_,\/_\_/_\     Author:   Pablo González de Aledo (), pablo.aledo@gmail.com
+ *     \.\_/_\_/>    Company:  Universidad de Cantabria
+ *     '-'   '-'
+ * =====================================================================================
+ */
+
+int a = 5;
+
+int main(){
+	if(a < 3)
+		return 0;
+	else
+		return 1;
+}
