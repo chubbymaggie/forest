ba29afe795fac6aa71a3e898b6f3927a5b9d071f
mint <mint@17f328a0-2faa-4260-a3e8-86cdd9e0d718>
2013-08-05 15:56:31 +0000

    Ejemplo de división.
    El hecho de que el código se esté ejecutando realmente con unos valores y simbólicamente con otros puede dar problemas con las divisiones por cero/excepciones.
    
    Habría que poner un puntero a cada dato real para que el solver le ponga el valor correcto?.
    
    
    git-svn-id: file:///media/disk/repository/release/trunk@280 17f328a0-2faa-4260-a3e8-86cdd9e0d718

---
+++ test/simple/division/division.c
@@ -0,0 +1,28 @@
+/*
+ * =====================================================================================
+ * /
+ * |     Filename:  division.c
+ * |
+ * |  Description:  
+ * |
+ * |      Version:  1.0
+ * |      Created:  08/05/2013 11:20:09 AM
+ * |     Revision:  none
+ * |     Compiler:  gcc
+ * `-. .--------------------
+ *    Y
+ *    ,,  ,---,
+ *   (_,\/_\_/_\     Author:   Pablo González de Aledo (), pablo.aledo@gmail.com
+ *     \.\_/_\_/>    Company:  Universidad de Cantabria
+ *     '-'   '-'
+ * =====================================================================================
+ */
+
+
+int main() {
+	int a,b;
+	if(a/b == 4)
+		return 0;
+	else 
+		return 1;
+}
