b44bebe47831eb2129cd665f337b7aab86b89250
mint <mint@17f328a0-2faa-4260-a3e8-86cdd9e0d718>
2013-11-28 10:29:58 +0000

    Cambiada la implementación de strcmp para que el compilador no introduzca un xor
    git-svn-id: file:///media/disk/repository/release/trunk@476 17f328a0-2faa-4260-a3e8-86cdd9e0d718

--- test/stdlibs/getopt/getopt.c
+++ test/stdlibs/getopt/getopt.c
@@ -166,10 +166,17 @@ int strlen( char *str)
   return(s - str);
 }
 
-int strcmp(char *s1, char *s2)
+/*int strcmp(char *s1, char *s2)*/
+/*{*/
+    /*while((*s1 && *s2) && (*s1++ == *s2++));*/
+    /*return *(--s1) - *(--s2);*/
+/*}*/
+
+int strcmp(char* s1, char* s2)
 {
-    while((*s1 && *s2) && (*s1++ == *s2++));
-    return *(--s1) - *(--s2);
+    while(*s1 && (*s1==*s2))
+        s1++,s2++;
+    return *(unsigned char*)s1-*(unsigned char*)s2;
 }
 
 
