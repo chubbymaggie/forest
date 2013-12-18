acff0b84c419b0630e0d638472561271eef29aca
mint <mint@17f328a0-2faa-4260-a3e8-86cdd9e0d718>
2013-12-18 09:46:39 +0000

    Added testcase of dereferencing a pointer with variable offset
    git-svn-id: file:///media/disk/repository/release/trunk@527 17f328a0-2faa-4260-a3e8-86cdd9e0d718

---
+++ test/simple/deref_variable/deref_variable.c
@@ -0,0 +1,42 @@
+/*
+ * =====================================================================================
+ * /
+ * |     Filename:  deref_variable.c
+ * |
+ * |  Description:  
+ * |
+ * |      Version:  1.0
+ * |      Created:  12/18/2013 09:35:57 AM
+ * |     Revision:  none
+ * |     Compiler:  gcc
+ * `-. .--------------------
+ *    Y
+ *    ,,  ,---,
+ *   (_,\/_\_/_\     Author:   Pablo González de Aledo (), pablo.aledo@gmail.com
+ *     \.\_/_\_/>    Company:  Universidad de Cantabria
+ *     '-'   '-'
+ * =====================================================================================
+ */
+
+#ifdef KLEE 
+#include "/llvm-2.9/klee/include/klee/klee.h"
+#endif 
+
+
+
+int main() {
+	int a;
+	int array[] = {1,2,3,4,5};
+
+
+#ifdef KLEE
+	klee_make_symbolic(&a, sizeof(a), "a");
+#endif
+
+
+
+	if(array[a] == 3)
+		return 0;
+	else
+		return 1;
+}
