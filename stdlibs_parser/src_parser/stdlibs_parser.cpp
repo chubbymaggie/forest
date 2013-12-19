42020dc9f1ff069409c353b0a69efa4d144e5435
mint <mint@17f328a0-2faa-4260-a3e8-86cdd9e0d718>
2013-12-19 20:17:33 +0000

    Use the -ast-dump produced by clang to get ranges instead of using it as a library. Much less elegant, but might be enough for our purposes
    git-svn-id: file:///media/disk/repository/release/trunk@542 17f328a0-2faa-4260-a3e8-86cdd9e0d718

--- stdlibs_parser/src_parser/stdlibs_parser.cpp
+++ stdlibs_parser/src_parser/stdlibs_parser.cpp
@@ -1,90 +1,73 @@
-#include <cstdio>
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
 #include <string>
+#include <string.h>
 #include <sstream>
+#include <set>
+#include <assert.h>
 
-#include "clang/AST/ASTConsumer.h"
-#include "clang/AST/RecursiveASTVisitor.h"
-#include "clang/Basic/Diagnostic.h"
-#include "clang/Basic/FileManager.h"
-#include "clang/Basic/SourceManager.h"
-#include "clang/Basic/TargetOptions.h"
-#include "clang/Basic/TargetInfo.h"
-#include "clang/Frontend/CompilerInstance.h"
-#include "clang/Lex/Preprocessor.h"
-#include "clang/Parse/ParseAST.h"
-#include "clang/Rewrite/Core/Rewriter.h"
-#include "clang/Rewrite/Frontend/Rewriters.h"
-#include "llvm/Support/Host.h"
-#include "llvm/Support/raw_ostream.h"
-
-using namespace clang;
 using namespace std;
 
+vector<string> funcs_to_extract;
+vector<string> structs_to_extract;
 
-vector<SourceRange> ranges;
-set<string> funcs_to_extract;
+typedef struct Location{
+	int row;
+	int column;
+} Location;
 
-// By implementing RecursiveASTVisitor, we can specify which AST nodes
-// we're interested in by overriding relevant methods.
-class MyASTVisitor : public RecursiveASTVisitor<MyASTVisitor>
-{
-	public:
-		MyASTVisitor(Rewriter &R)
-			: TheRewriter(R)
-		{}
-
-
-		bool VisitFunctionDecl(FunctionDecl *f) {
-			// Only function definitions (with bodies), not declarations.
-			if (f->hasBody()) {
+typedef struct Range {
+	Location start;
+	Location end;
+} Range;
 
 
-				// Function name
-				DeclarationName DeclName = f->getNameInfo().getName();
-				string FuncName = DeclName.getAsString();
+vector<Range> get_range_functions(){
 
-				if( funcs_to_extract.find(FuncName) != funcs_to_extract.end() ){
+	vector<Range> ret;
 
+	for( vector<string>::iterator it = funcs_to_extract.begin(); it != funcs_to_extract.end(); it++ ){
+		stringstream command;
+		command << "cd /tmp/;";
+		command << "cat ast | grep FunctionDecl | egrep '> \\<" << *it << "\\>' | grep -v '/usr/'";
+		command << " > ast_filter";
+		system(command.str().c_str());
 
-					ranges.push_back(f->getSourceRange());
+		FILE *file = fopen ( "/tmp/ast_filter", "r" );
+		char line [ 512 ]; /* or other suitable maximum line size */
+		vector<string> lines;
+		
+		while ( fgets ( line, sizeof(line), file ) != NULL ){
+			line[strlen(line)-1] = 0;
+			lines.push_back(string(line));
 
-					TheRewriter.InsertText(f->getLocStart(), "");
-				}
-
-			}
-
-			return true;
 		}
+		fclose ( file );
 
-	private:
+		assert(lines.size() == 1);
 
-		Rewriter &TheRewriter;
-};
+		command.str("");
+		
 
+		FILE *fp;
+		char ret[128];
+		//command << "cat /tmp/ast_filter | sed 's/[^:]*:\([^:]*\):\([^,]*\),[^:]*:\([^:]*\):\([^>]*\)>.*/\1 \2 \3 \4/g'";
+		command << "cat /tmp/ast_filter | sed 's/[^:]*:\\([^:]*\\):\\([^,]*\\),[^:]*:\\([^:]*\\):\\([^>]*\\)>.*/\\1 \\2 \\3 \\4/g'";
+		
+		int r1, c1, r2, c2;
+		fp = popen(command.str().c_str(), "r");
+		fscanf(fp, "%d %d %d %d", &r1, &c1, &r2, &c2);
+		pclose(fp);
 
-// Implementation of the ASTConsumer interface for reading an AST produced
-// by the Clang parser.
-class MyASTConsumer : public ASTConsumer
-{
-	public:
-		MyASTConsumer(Rewriter &R)
-			: Visitor(R)
-		{}
+		printf("%d %d %d %d\n", r1, c1, r2, c2);
+		
+		
+	}
 
-		// Override the method that gets called for each parsed top-level
-		// declaration.
-		virtual bool HandleTopLevelDecl(DeclGroupRef DR) {
-			for (DeclGroupRef::iterator b = DR.begin(), e = DR.end();
-					b != e; ++b)
-				// Traverse the declaration using our AST visitor.
-				Visitor.TraverseDecl(*b);
-			return true;
-		}
-
-	private:
-		MyASTVisitor Visitor;
-};
-
+	return ret;
+	
+}
 
 int main() {
 
@@ -104,7 +87,12 @@ int main() {
 		for( vector<string>::iterator it = files.begin(); it != files.end(); it++ ){
 			command << " " << *it;
 		}
-		command << "> /tmp/std_files";
+		command << "> /tmp/std_files.c";
+		system(command.str().c_str());
+
+		command.str("");
+		command << "cd /tmp/;";
+		command << "clang -Xclang -ast-dump -fno-color-diagnostics std_files.c > ast 2>/dev/null";
 		system(command.str().c_str());
 	}
 
@@ -114,66 +102,14 @@ int main() {
 
 		while ( fgets ( line, sizeof(line), file ) != NULL ){
 			line[strlen(line)-1] = 0;
-			funcs_to_extract.insert(string(line));
+			funcs_to_extract.push_back(string(line));
 		}
 		fclose ( file );
 	}
 
+	vector<Range> functions = get_range_functions();
 
 
-
-	// CompilerInstance will hold the instance of the Clang compiler for us,
-	// managing the various objects needed to run the compiler.
-	CompilerInstance* TheCompInst = new CompilerInstance();
-	//TheCompInst.createDiagnostics(0, 0);
-	TheCompInst->createDiagnostics();
-
-	// Initialize target info with the default triple for our platform.
-	TargetOptions TO;
-	TO.Triple = llvm::sys::getDefaultTargetTriple();
-	TargetInfo *TI = TargetInfo::CreateTargetInfo(
-			TheCompInst->getDiagnostics(), &TO);
-	TheCompInst->setTarget(TI);
-
-	TheCompInst->createFileManager();
-	FileManager &FileMgr = TheCompInst->getFileManager();
-	TheCompInst->createSourceManager(FileMgr);
-	SourceManager &SourceMgr = TheCompInst->getSourceManager();
-	TheCompInst->createPreprocessor();
-	TheCompInst->createASTContext();
-
-	// A Rewriter helps us manage the code rewriting task.
-	Rewriter TheRewriter;
-	TheRewriter.setSourceMgr(SourceMgr, TheCompInst->getLangOpts());
-
-	// Set the main file handled by the source manager to the input file.
-	const FileEntry *FileIn = FileMgr.getFile("/tmp/std_files");
-	SourceMgr.createMainFileID(FileIn);
-	TheCompInst->getDiagnosticClient().BeginSourceFile(
-			TheCompInst->getLangOpts(),
-			&(TheCompInst->getPreprocessor()));
-
-	// Create an AST consumer instance which is going to get called by
-	// ParseAST.
-	MyASTConsumer TheConsumer(TheRewriter);
-
-	// Parse the file to AST, registering our consumer as the AST consumer.
-	ParseAST(TheCompInst->getPreprocessor(), &TheConsumer,
-			TheCompInst->getASTContext());
-
-	// At this point the rewriter's buffer should be full with the rewritten
-	// file contents.
-	const RewriteBuffer *RewriteBuf =
-		TheRewriter.getRewriteBufferFor(SourceMgr.getMainFileID());
-
-	for ( unsigned int i = 0; i < ranges.size(); i++) {
-
-		unsigned int begin = ranges[i].getBegin().getRawEncoding()-2;
-		unsigned int end   = ranges[i].getEnd().getRawEncoding()-1;
-
-		printf("%s\n\n", string(RewriteBuf->begin(), RewriteBuf->end()).substr(begin, end-begin).c_str() );
-
-	}
 
 	return 0;
 }
