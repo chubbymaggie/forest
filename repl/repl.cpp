394c533b793df174ef8697bce6e5707c136824ee
mint <mint@17f328a0-2faa-4260-a3e8-86cdd9e0d718>
2014-01-01 19:51:35 +0000

    Refactoring repl
    git-svn-id: file:///media/disk/repository/release/trunk@557 17f328a0-2faa-4260-a3e8-86cdd9e0d718

--- repl/repl.cpp
+++ repl/repl.cpp
@@ -2,39 +2,54 @@
 #include <string.h>
 
 void init_wins(WINDOW **wins, int n);
-void win_show(WINDOW *win, char *label, int label_color);
-void win_show_2(WINDOW *win, char *label, int label_color);
+void win_show_title(WINDOW *win, char *label);
+void win_show_box(WINDOW *win);
 void print_in_middle(WINDOW *win, int starty, int startx, int width, char *string, chtype color);
 
-int main()
-{	WINDOW *my_wins[4];
-	PANEL  *my_panels[4];
-	PANEL  *top;
-	int ch;
 
+WINDOW *wins[4];    // Windows
+PANEL  *my_panels[4];  // Panels
+PANEL  *top;           // Top panel
+int ch;                // Character typed
+
+void initialize(){
 	/* Initialize curses */
 	initscr();
 	start_color();
 	cbreak();
-	noecho();
 	keypad(stdscr, TRUE);
 
 	/* Initialize all the colors */
 	init_pair(3, COLOR_BLUE, COLOR_BLACK);
+}
 
-	init_wins(my_wins, 4);
-	
+void initialize_panels(){
+
 	/* Attach a panel to each window */
-	my_panels[0] = new_panel(my_wins[0]);
-	my_panels[1] = new_panel(my_wins[1]);
-	my_panels[2] = new_panel(my_wins[2]);
-	my_panels[3] = new_panel(my_wins[3]);
+	my_panels[0] = new_panel(wins[0]);
+	my_panels[1] = new_panel(wins[1]);
+	my_panels[2] = new_panel(wins[2]);
+	my_panels[3] = new_panel(wins[3]);
 
 	/* Set up the user pointers to the next panel */
 	set_panel_userptr(my_panels[0], my_panels[1]);
 	set_panel_userptr(my_panels[1], my_panels[2]);
 	set_panel_userptr(my_panels[2], my_panels[3]);
 	set_panel_userptr(my_panels[3], my_panels[0]);
+}
+
+void initialize_wins() {
+	int x, y, i;
+	char label[80];
+
+	wins[0] = newwin(LINES-5      , COLS*3/4, 0          , 0)        ; win_show_box  (wins[0]) ;
+	wins[1] = newwin((LINES-5)/2  , COLS*1/4, 0          , COLS*3/4) ; win_show_title(wins[1], "Variables") ;
+	wins[2] = newwin((LINES-5)/2+1, COLS*1/4, (LINES-5)/2, COLS*3/4) ; win_show_title(wins[2], "Counter Example") ;
+	wins[3] = newwin(5            , COLS    , LINES-5    , 0)        ; win_show_box  (wins[3]) ;
+
+}
+
+void update(){
 
 	/* Update the stacking order. 2nd panel will be on top */
 	update_panels();
@@ -42,6 +57,18 @@ int main()
 	/* Show it on the screen */
 	doupdate();
 
+}
+
+int main(){
+
+	initialize();
+
+	initialize_wins();
+
+	initialize_panels();
+
+	update();
+	
 	top = my_panels[2];
 	while((ch = getch()) != 27)
 	{	switch(ch)
@@ -50,27 +77,14 @@ int main()
 				top_panel(top);
 				break;
 		}
-		update_panels();
-		doupdate();
+		update();
 	}
 	endwin();
 	return 0;
 }
 
-/* Put all the windows */
-void init_wins(WINDOW **wins, int n)
-{	int x, y, i;
-	char label[80];
 
-	wins[0] = newwin(LINES-5       , COLS*3/4 , 0           , 0)        ; sprintf(label , "Main Window") ; win_show_2(wins[0] , label , 3) ;
-	wins[1] = newwin((LINES-5)/2   , COLS*1/4 , 0           , COLS*3/4) ; sprintf(label , "Variables")   ; win_show(wins[1]   , label , 3) ;
-	wins[2] = newwin((LINES-5)/2+1 , COLS*1/4 , (LINES-5)/2 , COLS*3/4) ; sprintf(label , "Solution")    ; win_show(wins[2]   , label , 3) ;
-	wins[3] = newwin(5             , COLS     , LINES-5     , 0)        ; sprintf(label , "REPL")        ; win_show_2(wins[3] , label , 3) ;
-
-}
-
-/* Show the window with a border and a label */
-void win_show_2(WINDOW *win, char *label, int label_color)
+void win_show_box(WINDOW *win)
 {	int startx, starty, height, width;
 
 	getbegyx(win, starty, startx);
@@ -79,9 +93,9 @@ void win_show_2(WINDOW *win, char *label, int label_color)
 	box(win, 0, 0);
 }
 
-/* Show the window with a border and a label */
-void win_show(WINDOW *win, char *label, int label_color)
-{	int startx, starty, height, width;
+void win_show_title(WINDOW *win, char *label) {
+
+	int startx, starty, height, width;
 
 	getbegyx(win, starty, startx);
 	getmaxyx(win, height, width);
@@ -91,11 +105,11 @@ void win_show(WINDOW *win, char *label, int label_color)
 	mvwhline(win, 2, 1, ACS_HLINE, width - 2);
 	mvwaddch(win, 2, width - 1, ACS_RTEE);
 	
-	print_in_middle(win, 1, 0, width, label, COLOR_PAIR(label_color));
+	print_in_middle(win, 1, 0, width, label, COLOR_PAIR(3));
 }
 
-void print_in_middle(WINDOW *win, int starty, int startx, int width, char *string, chtype color)
-{	int length, x, y;
+void print_in_middle(WINDOW *win, int starty, int startx, int width, char *string, chtype color) {
+	int length, x, y;
 	float temp;
 
 	if(win == NULL)
