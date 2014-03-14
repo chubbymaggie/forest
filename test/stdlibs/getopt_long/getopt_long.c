160b16f06e5cfb7447cc8595303d4d0c4edfe8df
mint <mint@17f328a0-2faa-4260-a3e8-86cdd9e0d718>
2014-03-14 12:32:40 +0000

    Added configuration file and simplified getopt_long
    git-svn-id: file:///media/disk/repository/release/trunk@736 17f328a0-2faa-4260-a3e8-86cdd9e0d718

--- test/stdlibs/getopt_long/getopt_long.c
+++ test/stdlibs/getopt_long/getopt_long.c
@@ -18,26 +18,25 @@
  * =====================================================================================
  */
 
-// Example from http://linuxprograms.wordpress.com/2012/06/22/c-getopt_long-example-accessing-command-line-arguments/
+// Example from http://linuxprograms.wordpress.com/2012/06/22/c-get opt_long-example-accessing-command-line-arguments/
 
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <getopt.h>
 
-/** Program to calculate the area and perimeter of 
- * a rectangle using command line arguments
- */
-void print_usage() {
-	printf("Usage: rectangle [ap] -l num -b num\n");
-}
+extern "C" int getopt_long(int argc, char * const argv[], const char *optstring, const struct option *longopts, int *longindex);
 
-int main(int argc, char *argv[]) {
-	int opt= 0;
-	int area = -1, perimeter = -1, breadth = -1, length =-1;
+#define	no_argument		0
+#define required_argument	1
+#define optional_argument	2
 
-	//Specifying the expected options
-	//The two options l and b expect numbers as argument
+struct option
+{
+  char *name;
+  int has_arg;
+  int *flag;
+  int val;
+};
+
+int main() {
+
 	static struct option long_options[] = {
 		{"area",      no_argument,       0,  'a' },
 		{"perimeter", no_argument,       0,  'p' },
@@ -46,37 +45,17 @@ int main(int argc, char *argv[]) {
 		{0,           0,                 0,  0   }
 	};
 
-	int long_index =0;
-	while ((opt = getopt_long(argc, argv,"apl:b:", 
-					long_options, &long_index )) != -1) {
-		switch (opt) {
-			case 'a' : area = 0;
-				   break;
-			case 'p' : perimeter = 0;
-				   break;
-			case 'l' : length = atoi(optarg); 
-				   break;
-			case 'b' : breadth = atoi(optarg);
-				   break;
-			default: print_usage(); 
-				 exit(EXIT_FAILURE);
-		}
-	}
-	if (length == -1 || breadth ==-1) {
-		print_usage();
-		exit(EXIT_FAILURE);
-	}
+	char* str_1 = "#########";
+	char* str_2 = "aaaaaaaaa";
+	char* ar[2];
+	ar[0] = str_1;
+	ar[1] = str_2;
 
-	// Calculate the area
-	if (area == 0) {
-		area = length * breadth;
-		printf("Area: %d\n",area);
-	}
 
-	// Calculate the perimeter
-	if (perimeter == 0) {
-		perimeter = 2 * (length + breadth);
-		printf("Perimeter: %d\n",perimeter);
-	}
-	return 0;
+
+	int long_index = 0;
+	if( getopt_long(2, ar,"apl:b:", long_options, &long_index ) == 'a' )
+		return 1;
+	else
+		return 0;
 }
