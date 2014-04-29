0caf3c1545b394e316fbc03357601d7f001c930b
mint <mint@17f328a0-2faa-4260-a3e8-86cdd9e0d718>
2014-04-29 09:53:28 +0000

    Double solver cleaning
    git-svn-id: file:///media/disk/repository/release/trunk@857 17f328a0-2faa-4260-a3e8-86cdd9e0d718

--- back-end/z3_double.cpp
+++ back-end/z3_double.cpp
@@ -54,7 +54,7 @@ void Z3Double::dump_problem(string& filename_base){
 		FILE* file = fopen(filename_base.c_str(), "w");
 		Z3RealInt::dump_header(file);
 		Z3RealInt::dump_variables(file);
-		//dump_extra(file);
+		Z3RealInt::dump_extra(file);
 		dump_conditions(file);
 		dump_check_sat(file);
 		dump_get(file);
@@ -69,7 +69,6 @@ void Z3Double::dump_problem(string& filename_base){
 		FILE* file = fopen(filename_base.c_str(), "w");
 		Z3BitVector::dump_header(file);
 		Z3BitVector::dump_variables(file);
-		//dump_extra(file);
 		dump_conditions(file);
 		dump_check_sat(file);
 		dump_get(file);
@@ -80,90 +79,56 @@ void Z3Double::dump_problem(string& filename_base){
 }
 
 void Z3Double::dump_header(FILE* file){
-
-	//Z3RealInt::dump_header(file);
-	//Z3BitVector::dump_header(file);
-
 }
 
 void Z3Double::dump_variables(FILE* file){
-
-	//Z3RealInt::dump_variables(file);
-	//Z3BitVector::dump_variables(file);
-	
-
 }
 
 void Z3Double::right_shift(string op1, string op2, string dst, stringstream& content_ss){
-
-	Z3RealInt::right_shift(op1, op2, dst, content_ss);
 	Z3BitVector::right_shift(op1, op2, dst, content_ss);
-
 }
 
 void Z3Double::left_shift(string op1, string op2, string dst, stringstream& content_ss){
-
-	Z3RealInt::left_shift(op1, op2, dst, content_ss);
 	Z3BitVector::left_shift(op1, op2, dst, content_ss);
-
 }
 
 void Z3Double::and_operation(string op1, string op2, string dst, stringstream& content_ss){
-
-
-	Z3RealInt::and_operation(op1, op2, dst, content_ss);
 	Z3BitVector::and_operation(op1, op2, dst, content_ss);
-
 }
 
 void Z3Double::or_operation(string op1, string op2, string dst, stringstream& content_ss){
-
-	Z3RealInt::or_operation(op1, op2, dst, content_ss);
 	Z3BitVector::or_operation(op1, op2, dst, content_ss);
-
 }
 
 string Z3Double::canonical_representation(string in){
-
-	Z3RealInt::canonical_representation(in);
 	return Z3BitVector::canonical_representation(in);
 }
 
 string Z3Double::internal_representation(int in, string type){
-	 Z3RealInt::internal_representation(in, type);
 	return Z3BitVector::internal_representation(in, type);
 }
 
 string Z3Double::name_operation(string operation){
-	Z3RealInt::name_operation(operation);
 	return Z3BitVector::name_operation(operation);
 }
 
 
 void Z3Double::xor_operation(string op1, string op2, string dst, stringstream& content_ss){
-	Z3RealInt::xor_operation(op1, op2, dst, content_ss);
 	return Z3BitVector::xor_operation(op1, op2, dst, content_ss);
 }
 
 
 
 void Z3Double::dump_extra(FILE* file){
-	Z3RealInt::dump_extra(file);
-	return Z3BitVector::dump_extra(file);
 }
 
 
 void Z3Double::cast_instruction(string src, string dst, string type_src, string type_dst){
-
-	Z3RealInt::cast_instruction(src, dst, type_src, type_dst);
 	return Z3BitVector::cast_instruction(src, dst, type_src, type_dst);
-	
 }
 
 
 map<set<pair<string, int> > , int > Z3Double::get_idx_val(string base,string idx_content, vector<string> indexes, int first_address, int last_address){
-
-	Z3RealInt::get_idx_val(base,idx_content,indexes, first_address, last_address);
 	return Z3BitVector::get_idx_val(base,idx_content,indexes, first_address, last_address);
 
 }
